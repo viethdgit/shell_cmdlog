053e807f9ccdfad729042cfb791f7e9435923cd5
viethdgit <uocthem1dieu@gmail.com>
2017-12-29 09:54:05 +0700

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#cmdlog
+grep -qF "PROMPT_COMMAND='RETRN_VAL=\$?;logger" /etc/bashrc || echo \
+'export PROMPT_COMMAND='\''RETRN_VAL=$?;logger -p local6.debug "[$(echo $SSH_CLIENT | cut -d" " -f1)] # $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" )"'\''' >> /etc/bashrc
+
+grep -qF "/var/log/cmdlog.log" /etc/rsyslog.conf || echo \
+"local6.debug                /var/log/cmdlog.log" >> /etc/rsyslog.conf
+
+service rsyslog restart
+chmod 644 /var/log/cmdlog.log
+
+cat > /etc/logrotate.d/cmdlog << EOF
+/var/log/cmdlog.log {
+	create 0644 root root
+	compress
+	weekly
+	rotate 12
+	sharedscripts
+	postrotate
+		/bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true
+	endscript
+}
+EOF
+
+#crontab
+chown root:root /etc/crontab
+chmod og-rwx /etc/crontab
+chown root:root /etc/cron.hourly
+chmod og-rwx /etc/cron.hourly
+chown root:root /etc/cron.daily
+chmod og-rwx /etc/cron.daily
+chown root:root /etc/cron.weekly
+chmod og-rwx /etc/cron.weekly
+chown root:root /etc/cron.monthly
+chmod og-rwx /etc/cron.monthly
+
+chown root:root /etc/cron.d
+chmod og-rwx /etc/cron.d
+rm /etc/cron.deny
+rm /etc/at.deny
+touch /etc/cron.allow
+touch /etc/at.allow
+chmod og-rwx /etc/cron.allow
+chmod og-rwx /etc/at.allow
+chown root:root /etc/cron.allow
+chown root:root /etc/at.allow
