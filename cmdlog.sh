e06b39e530d5f81a1a549b0e6e4f67e703146d87
viethdgit <uocthem1dieu@gmail.com>
2018-01-25 15:52:57 +0700

    Update cmdlog.sh

--- cmdlog.sh
+++ cmdlog.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #cmdlog
 grep -qF "PROMPT_COMMAND='RETRN_VAL=\$?;logger" /etc/bashrc || echo \
 'export PROMPT_COMMAND='\''RETRN_VAL=$?;logger -p local6.debug "[$(echo $SSH_CLIENT | cut -d" " -f1)] # $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" )"'\''' >> /etc/bashrc
